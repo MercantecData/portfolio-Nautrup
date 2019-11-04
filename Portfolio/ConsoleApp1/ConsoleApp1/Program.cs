119d4d9a5ae710c6f6cd3c31af627bffb13ec4b8
Jonas <jonasrj97@gmail.com>
2019-11-04 09:29:48 +0100

    Files added

---
+++ Portfolio/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            ArrayCounter();
+        }
+
+        static void ArrayCounter()
+        {
+            Console.WriteLine("Write a number : ");
+            int input = int.Parse(Console.ReadLine());
+            int[] arrayCount = new int[input];
+            foreach (var number in arrayCount)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
