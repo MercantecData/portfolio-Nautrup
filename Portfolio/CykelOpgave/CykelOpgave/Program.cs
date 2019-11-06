434c03011e0ffd7f3f5348ee99455d36223b6766
Jonas <jonasrj97@gmail.com>
2019-11-06 08:13:50 +0100

    Cykel opgave tilføjet

---
+++ Portfolio/CykelOpgave/CykelOpgave/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CykelOpgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
