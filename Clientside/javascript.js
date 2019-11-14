d7e752345f8788bf17715aeeebed728314616f80
Jonas <jonasrj97@gmail.com>
2019-11-14 12:59:46 +0100

    Javascript added

--- Clientside/javascript.js
+++ Clientside/javascript.js
@@ -1,25 +1,40 @@
-function ChangeMeText() {
+function ChangeMeText() 
+{
 	document.getElementById('meText').innerHTML = "Jeg hedder Tornado Toben og jeg er helt tosset med vindruer!";
 }
 
 
-function ChangeButtonText() {
+function ChangeButtonText() 
+{
 	document.getElementById("demo").innerHTML = "Vildt!";
 }
 //Adds new text to a paragraph
 document.getElementById("newElement").innerHTML = "Jeg er fra JavaScript!";
 
-function RemoveElement(obj) {
+function RemoveElement(obj) 
+{
 	document.getElementById("btnRemove").remove();
-	
 }
 
-
 var fruit = ["Apple","Banana","Pear"];
-for (var i = 0; i < fruit.length; i++) {
+for (var i = 0; i < fruit.length; i++) 
+{
 	document.getElementById("fruitArray").innerHTML += fruit[i]+ "<br>";
 }
 
-function MakeElementWithTextBox() {
 
+function MakeElementWithTextBox() 
+{
+	var getName = document.getElementById("tbGuestName").value;
+	var getMessage = document.getElementById("tbGuestMessage").value;
+	if (getMessage == "") 
+	{
+		alert("Du skal skrive en besked!");
+	} else if (getName == "") 
+	{
+		getName == "unknown";
+	}
+
+	document.getElementById("printTbName").innerHTML = getName +"<br>"+ getMessage + "<hr>";
+	
 }
